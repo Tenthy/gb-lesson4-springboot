88979e6029bb79c8bb264c09569b4504000366bc
Kmetha <rakursdead@gmail.com>
2022-04-10 14:54:47 +0300

    Initial commit

---
+++ src/main/java/ru/kmetha/springbootapril/JavaSpringbootAprilApplication.java
@@ -0,0 +1,13 @@
+package ru.kmetha.springbootapril;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootAprilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringbootAprilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kmetha/springbootapril/JavaSpringbootAprilApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kmetha.springbootapril;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootAprilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
